f6e7c44be58feea81e81934146e2e3484470c3f2
Steve Strong <sstrong@apprenticesystems.com>
2018-05-27 14:12:07 -0400

    a

---
+++ WebAPI.Dockerfile
@@ -0,0 +1,24 @@
+FROM microsoft/aspnetcore:2.0 AS base
+WORKDIR /app
+EXPOSE 80
+
+
+FROM microsoft/aspnetcore-build:2.0 AS build
+WORKDIR /src
+COPY WebAPI.sln ./
+COPY WebAPI/WebAPI.csproj WebAPI/
+RUN dotnet restore -nowarn:msb3202,nu1503
+COPY . .
+WORKDIR /src/WebAPI
+RUN dotnet build -c Release -o /app
+
+FROM build AS publish
+RUN dotnet publish -c Release -o /app
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app .
+ENTRYPOINT ["dotnet", "WebAPI.dll"]
+
+# docker build -t webapi -f WebAPI.Dockerfile .
+# docker run -d -p 3001:80 --name webapi webapi
---
+++ WebAPI/Controllers/OtherController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+
+
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class OtherController : Controller
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        private static async Task<List<String>> OtherAPI()
+        {
+            var serializer = new DataContractJsonSerializer(typeof(List<String>));
+
+            client.DefaultRequestHeaders.Accept.Clear();
+            //client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+           // var stringTask = client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
+            //var streamTask = client.GetStreamAsync("http://localhost:3002/api/OtherValues");
+            var streamTask = client.GetStreamAsync("/webapiother");           var results = serializer.ReadObject(await streamTask) as List<String>;
+            return results;
+        }
+
+        [HttpGet]
+        public IEnumerable<String> Get()
+        {
+            var results = OtherAPI().Result;
+
+            return results;
+        }
+    }
+}
